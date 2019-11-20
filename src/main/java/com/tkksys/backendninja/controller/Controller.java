998cd1719a71c6940507f0ef83cafc3528cfe9d1
Wilson Ferreira <wirxon@gmail.com>
2019-11-20 08:17:26 -0300

    Primera clase de prueba

---
+++ src/main/java/com/tkksys/backendninja/controller/Controller.java
@@ -0,0 +1,20 @@
+/**
+ * <h1>Cabecera de la documentacion</h1>
+ * Cuerpo de la documentacion
+ *
+ * @author wilson ferreira
+ * @version 1.0.0
+ * @date 20 nov. 2019
+ * @since 1.0.0
+ * @lastupdate 20 nov. 2019 - 08:15:55
+ * 
+ */
+ 
+package com.tkksys.backendninja.controller;
+
+
+public class Controller {
+	public String primerController() {
+		return "soy un controller";
+	}
+}
