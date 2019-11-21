6551e1da5951fe8173b90911ad35e07dab2c665f
Wilson Ferreira <wirxon@gmail.com>
2019-11-21 08:43:46 -0300

    4.9.Recibiendo-una-peticion-GET-1

---
+++ src/main/java/com/tkksys/backendninja/controller/My2Controller.java
@@ -0,0 +1,26 @@
+package com.tkksys.backendninja.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.tkksys.backendninja.util.Util;
+
+@Controller
+@RequestMapping("/demos2")
+public class My2Controller extends Util{
+
+	/**
+	 * Primera forma de realizar un request de tipo GET
+	 * localhost:8080/demos2/request1?nm=JON
+	 * @return
+	 */
+	@GetMapping("/request1")
+	public ModelAndView request1(@RequestParam(name = "nm", required = false, defaultValue = "NULL") String name) {
+		ModelAndView mav = new ModelAndView(PAGE_EXAMPLE2);
+		mav.addObject("nm_in_model", name);
+		return mav;
+	}
+}
--- src/main/java/com/tkksys/backendninja/util/Util.java
+++ src/main/java/com/tkksys/backendninja/util/Util.java
@@ -2,4 +2,5 @@ package com.tkksys.backendninja.util;
 
 public class Util {
 	public static final String PAGE_EXAMPLE = "paginaDemo";
+	public static final String PAGE_EXAMPLE2 = "paginaDemo2";
 }
