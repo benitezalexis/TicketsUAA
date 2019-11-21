703a9d8be32f2a08814a35fa31ac60d9a42dd0f1
Wilson Ferreira <wirxon@gmail.com>
2019-11-21 08:56:10 -0300

    4.10.Recibiendo-una-peticion-GET-2

--- src/main/java/com/tkksys/backendninja/controller/My2Controller.java
+++ src/main/java/com/tkksys/backendninja/controller/My2Controller.java
@@ -2,6 +2,7 @@ package com.tkksys.backendninja.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,11 +15,23 @@ public class My2Controller extends Util{
 
 	/**
 	 * Primera forma de realizar un request de tipo GET
-	 * localhost:8080/demos2/request1?nm=JON
+	 * "localhost:8080/demos2/request1?nm=JON"
 	 * @return
 	 */
 	@GetMapping("/request1")
 	public ModelAndView request1(@RequestParam(name = "nm", required = false, defaultValue = "NULL") String name) {
+		ModelAndView mav = new ModelAndView(PAGE_EXAMPLE2);
+		mav.addObject("nm_in_model", name);
+		return mav;
+	}
+	
+	/**
+	 * Segunda forma de realizar un request de tipo GET
+	 * "localhost:8080/demos2/request2/JON"
+	 * @return
+	 */
+	@GetMapping("/request2/{nm}")
+	public ModelAndView request2(@PathVariable("nm") String name) {
 		ModelAndView mav = new ModelAndView(PAGE_EXAMPLE2);
 		mav.addObject("nm_in_model", name);
 		return mav;
