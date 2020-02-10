ef932cb638a8c96231ec1290f3baf26ae7751a1a
Wilson Ferreira <wirxon@gmail.com>
2020-02-10 17:28:09 -0300

    8.2.Creando-las-entidades-necesarias

---
+++ src/main/java/com/tkksys/backendninja/entity/User.java
@@ -0,0 +1,84 @@
+package com.tkksys.backendninja.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue()
+	@Column(name = "username", unique = true, nullable = false, length = 45)
+	private String username;
+	
+	@Column(name = "password", nullable = true, length = 60)
+	private String password;
+	
+	@Column(name = "enabled", nullable = false)
+	private boolean enabled;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+	private Set<UserRol> userRole = new HashSet<>();
+
+	public User(String username, String password, boolean enabled) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+	}
+
+	public User(String username, String password, boolean enabled, Set<UserRol> userRole) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.userRole = userRole;
+	}
+
+	public User() {
+		super();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Set<UserRol> getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(Set<UserRol> userRole) {
+		this.userRole = userRole;
+	}
+
+}
