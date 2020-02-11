cda1593cc12f4a1f63a37e08f730c1c540de883f
Wilson Ferreira <wirxon@gmail.com>
2020-02-11 19:15:14 -0300

    9.1.Guardando-logs-de-peticio-n-en-base-de-datos

--- src/main/java/com/tkksys/backendninja/component/RequestTimeInterceptor.java
+++ src/main/java/com/tkksys/backendninja/component/RequestTimeInterceptor.java
@@ -1,15 +1,28 @@
 package com.tkksys.backendninja.component;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.tkksys.backendninja.repository.LogRepository;
+
 @Component("requestTimeInterceptor")
 public class RequestTimeInterceptor extends HandlerInterceptorAdapter{
+	
+	@Autowired
+	@Qualifier("logRepository")
+	private LogRepository logRepository;
+	
 	private static final Log LOG = LogFactory.getLog(RequestTimeInterceptor.class);
 	
 	/**
@@ -26,7 +39,17 @@ public class RequestTimeInterceptor extends HandlerInterceptorAdapter{
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 		long starTime = (long) request.getAttribute("starTime");
-		LOG.info("URL to: '"+request.getRequestURL().toString() + "' in: '"+ (System.currentTimeMillis() - starTime) + "ms'");
+		String url = request.getRequestURL().toString();
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String username = "";
+		if(auth != null && auth.isAuthenticated()) {
+			username = auth.getName();
+			
+		}
+		
+		logRepository.save(new com.tkksys.backendninja.entity.Log(new Date(), auth.getDetails().toString(), username, url));
+		LOG.info("URL to: '"+ url + "' in: '"+ (System.currentTimeMillis() - starTime) + "ms'");
 	}
 	
 	
